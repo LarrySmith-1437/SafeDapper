29002933f8c952be01ae57da8252556952fdbeef
Lawrence Smith <lsmith1437@cox.net>
2017-10-04 12:13:09 -0700

    Adding SafeQuerySingle
    Pretty much any time we need a safe version of an existing Dapper call we can extend it in the same way.

--- SafeDapper/DapperExtensions.cs
+++ SafeDapper/DapperExtensions.cs
@@ -33,5 +33,26 @@ namespace SafeDapper
 
             return cnn.Query<T>(sql, param, transaction, buffered, commandTimeout, commandType);
         }
+
+
+        /// <summary>
+        /// Extension to the Dapper methods, SafeQuery will throw an exception if any column in the query 
+        /// is not mapped to a property of the type. This prevents silent failure/defaulted property values
+        /// in the case that the names of columns/properties are misspelled, or changed in one place but not another.
+        /// </summary>
+        public static T SafeQuerySingle<T>(this IDbConnection cnn, string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            lock (_lock)
+            {
+                if (TypesThatHaveMapper.ContainsKey(typeof(T)) == false)
+                {
+                    SqlMapper.SetTypeMap(typeof(T), new ThrowWhenNullTypeMap<T>());
+                    TypesThatHaveMapper.Add(typeof(T), null);
+                }
+            }
+
+            return cnn.QuerySingle<T>(sql, param, transaction, commandTimeout, commandType);
+        }
+
     }
 }
---
+++ SafeDapperTest/SafeQuerySingleTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.CodeDom;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using SafeDapper;
+
+/// <summary>
+/// If you don't have MS office installed, you'll likely need to install the office drivers to read the excel sheet test file w/out having actual office installed
+/// https://www.microsoft.com/en-us/download/confirmation.aspx?id=23734
+/// or included in this checkin, a version that works on windows 7 x64 through Windows 10.
+/// </summary>
+
+namespace SafeDapperTest
+{
+    [TestClass]
+    public class SafeQuerySingleTests
+    {
+        private string _testFile = "TestData.xlsx";
+        [TestMethod]
+        [ExpectedException(typeof(DapperObjectMappingException))]
+        public void SafeQuerySingleThrowsExceptionWhenColumnNotMapped()
+        {
+            var currentAssemblyDirectory =Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); // System.Reflection.Assembly.GetEntryAssembly();
+            var fullPath = Path.Combine(currentAssemblyDirectory, _testFile);
+            var connString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"{fullPath}\";Extended Properties=\"Excel 12.0 Xml;HDR=Yes;IMEX=1;\"";
+
+            var exists = File.Exists(fullPath);
+            using (var conn = new OleDbConnection(connString))
+            {
+                conn.Open();
+                var result = conn.SafeQuerySingle<TestDataPropertyMismatch>($"Select top 1 * from [Sheet1$]", null, null,
+                    commandType: CommandType.Text);
+
+            }
+        }
+
+        [TestMethod]
+        public void SafeQuerySingleExecutesAndGetsData()
+        {
+            var currentAssemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); // System.Reflection.Assembly.GetEntryAssembly();
+            var fullPath = Path.Combine(currentAssemblyDirectory, _testFile);
+            var connString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"{fullPath}\";Extended Properties=\"Excel 12.0 Xml;HDR=Yes;IMEX=1;\"";
+
+            var exists = File.Exists(fullPath);
+            using (var conn = new OleDbConnection(connString))
+            {
+                conn.Open();
+                var result = conn.SafeQuerySingle<TestData>($"Select top 1 * from [Sheet1$]", null, null,
+                    commandType: CommandType.Text);
+
+                Assert.IsNotNull(result);
+                Assert.AreEqual(1, result.ColumnA);
+                Assert.AreEqual(2, result.ColumnB);
+                Assert.AreEqual(3, result.ColumnC);            }
+        }
+
+    }
+}
