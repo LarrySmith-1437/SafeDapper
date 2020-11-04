df2ddac4930bcb092d5d2b934c389cf576d6c5aa
Robert Sirre <robert@cs.nl>
2020-11-04 01:06:24 +0100

    Minor cleanup and performance  gains
    Prevented unnecessary locking, reused and exposed type map registration

--- SafeDapper/DapperExtensions.cs
+++ SafeDapper/DapperExtensions.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Data;
-using Dapper;
 
 /*
  * from:  http://stackoverflow.com/questions/28678442/how-can-i-make-dapper-net-throw-when-result-set-has-unmapped-columns/39490419#39490419
@@ -10,9 +10,9 @@ using Dapper;
 
 namespace SafeDapper
 {
-    public  static class DapperExtensions
+    public static class DapperExtensions
     {
-        private static readonly IDictionary<Type, object> TypesThatHaveMapper = new Dictionary<Type, object>();
+        private static readonly HashSet<Type> TypesThatHaveMapper = new HashSet<Type>();
         private static readonly object _lock = new object();
 
         /// <summary>
@@ -21,17 +21,11 @@ namespace SafeDapper
         /// in the case that the names of columns/properties are misspelled, or changed in one place but not another.
         /// </summary>
         public static IEnumerable<T> SafeQuery<T>(this IDbConnection cnn, string sql, object param = null,
-		    IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = default(int?),
-		    CommandType? commandType = default(CommandType?))
+            IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = default(int?),
+            CommandType? commandType = default)
         {
-            lock (_lock)
-            {
-                if (TypesThatHaveMapper.ContainsKey(typeof(T)) == false)
-                {
-                    SqlMapper.SetTypeMap(typeof(T), new ThrowWhenNullTypeMap<T>());
-                    TypesThatHaveMapper.Add(typeof(T), null);
-                }
-            }
+            AssertSafeTypeMap<T>();
+
             return cnn.Query<T>(sql, param, transaction, buffered, commandTimeout, commandType);
         }
 
@@ -43,16 +37,33 @@ namespace SafeDapper
         /// </summary>
         public static T SafeQuerySingle<T>(this IDbConnection cnn, string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
         {
-            lock (_lock)
-            {
-                if (TypesThatHaveMapper.ContainsKey(typeof(T)) == false)
-                {
-                    SqlMapper.SetTypeMap(typeof(T), new ThrowWhenNullTypeMap<T>());
-                    TypesThatHaveMapper.Add(typeof(T), null);
-                }
-            }
+            AssertSafeTypeMap<T>();
 
             return cnn.QuerySingle<T>(sql, param, transaction, commandTimeout, commandType);
+        }
+
+        /// <summary>
+        /// Specifies a safe type mapper (<see cref="ThrowWhenNullTypeMap{T}"/>) for <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static void AssertSafeTypeMap<T>()
+        {
+            // First do cheap check without  locking
+            if (!TypesThatHaveMapper.Contains(typeof(T))){
+
+                // Then lock
+                lock (_lock)
+                {
+                    //And check again
+                    if (!TypesThatHaveMapper.Contains(typeof(T)))
+                    {
+                        SqlMapper.SetTypeMap(typeof(T), new ThrowWhenNullTypeMap<T>());
+
+                        TypesThatHaveMapper.Add(typeof(T));
+                    }
+                }
+            }
         }
 
     }
